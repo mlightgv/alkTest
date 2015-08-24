f6c978428a42ede72262472a9cbf9de722d8f72c
Mary Gomez Villarreal <marygomez@192-168-1-11.tpgi.com.au>
2015-08-25 00:54:28 +1000

    Implement YAML for report_spec.rb

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -1,52 +1,51 @@
 require 'statistic'
 require 'parse_csv'
 require 'report'
+require 'yaml'
 
 describe Report do
 
   before do
+    @yaml = YAML.load_file("./fixtures/report.yml")
     parse = ParseCSV.new(:file_in => "spec/ad_performance_aug.csv")
-    data = parse.parse_data
-    statistics = Statistic.new(:results => data)
-    columns = { "Campaign ID" => "id_campaing", "Final URL" => "final_url", "Day" => "day" }
-    @metric = { :row1 => "Highest Impressions", :row2 => "Highest Clicks", :row3 => "Highest Converted Clicks", :row4 => "Lowest Cost", :row5 => "Highest Conv. Clicks/Clicks", :row6 => "Lowest Cost / Converted Click"}
-    metrics = Hash.new
-    metrics = { @metric[:row1] => statistics.highest_impressions,
-                @metric[:row2] => statistics.highest_clicks,
-                @metric[:row3] => statistics.highest_converted_clicks, 
-                @metric[:row4] => statistics.lowest_cost,
-                @metric[:row5] => statistics.highest_converted_clicks_div_clicks, 
-                @metric[:row6] => statistics.lowest_cost_div_converted_click }
-        report = Report.new(:metrics => metrics, :columns => columns)
+    statistics = Statistic.new(:results => parse.parse_data)
+    columns = { @yaml["column_name2"] => "id_campaing", @yaml["column_name3"] => "final_url", @yaml["column_name4"] => "day" }
+    metrics = { @yaml["row_metric_name1"] => statistics.highest_impressions,
+                @yaml["row_metric_name2"] => statistics.highest_clicks,
+                @yaml["row_metric_name3"] => statistics.highest_converted_clicks, 
+                @yaml["row_metric_name4"] => statistics.lowest_cost,
+                @yaml["row_metric_name5"] => statistics.highest_converted_clicks_div_clicks, 
+                @yaml["row_metric_name6"] => statistics.lowest_cost_div_converted_click }
+    report = Report.new(:metrics => metrics, :columns => columns)
     @report = report.generate_report
   end
 
   it "Show Title" do
-    expect(@report["Metrics"]).to eql(["Campaign ID", "Final URL", "Day"])
+    expect(@report[@yaml["column_name1"]]).to eql(@yaml["headers"])
   end
 
   it "Show Highest Impressions" do
-    expect(@report[@metric[:row1]]).to eql(["243061000", "http://courses.alkhemyinstitute.edu.au/advanced-diploma-of-transpersonal-art-therapy/", "9/08/2015"])
+    expect(@report[@yaml["row_metric_name1"]]).to eql(@yaml["row_value1"])
   end
 
   it "Show Highest Clicks" do
-    expect(@report[@metric[:row2]]).to eql(["243061000", "http://courses.alkhemyinstitute.edu.au/advanced-diploma-of-transpersonal-art-therapy/", "6/08/2015"])
+    expect(@report[@yaml["row_metric_name2"]]).to eql(@yaml["row_value2"])
   end
 
   it "Show Highest Converted Clicks" do
-    expect(@report[@metric[:row3]]).to eql(["243061000", "http://courses.alkhemyinstitute.edu.au/advanced-diploma-of-transpersonal-art-therapy/", "6/08/2015"])
+    expect(@report[@yaml["row_metric_name3"]]).to eql(@yaml["row_value3"])
   end
 
   it "Show Lowest Cost" do
-    expect(@report[@metric[:row4]]).to eql(["243061000", "http://courses.alkhemyinstitute.edu.au/bachelor-of-holistic-counselling/", "12/08/2015"])
+    expect(@report[@yaml["row_metric_name4"]]).to eql(@yaml["row_value4"])
   end
 
   it "Show Highest Conv. Clicks/Clicks" do
-    expect(@report[@metric[:row5]]).to eql(["243061000", "http://courses.alkhemyinstitute.edu.au/advanced-diploma-of-transpersonal-art-therapy/", "3/08/2015"])
+    expect(@report[@yaml["row_metric_name5"]]).to eql(@yaml["row_value5"])
   end
 
   it "Show Lowest Cost / Converted Click" do
-    expect(@report[@metric[:row6]]).to eql(["243061000", "http://courses.alkhemyinstitute.edu.au/advanced-diploma-of-transpersonal-art-therapy/", "2/08/2015"])
+    expect(@report[@yaml["row_metric_name6"]]).to eql(@yaml["row_value6"])
   end
 
 end
