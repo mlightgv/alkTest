7c956d577f61d1d04c28fd2932bb3d78f24476f9
Mary Gomez Villarreal <marygomez@192-168-1-11.tpgi.com.au>
2015-08-24 17:25:35 +1000

    Refactoring of Statistic class

--- lib/statistic.rb
+++ lib/statistic.rb
@@ -7,27 +7,33 @@ attr_reader :results
   end
 
   def highest_impressions 
-    @results.select { |a| a[:id_campaing].strip != "--" }.max_by(&:impressions)
+    select_data.max_by(&:impressions)
   end
 
   def highest_clicks  
-    @results.select { |a| a[:id_campaing].strip != "--" }.max_by(&:clicks)
+    select_data.max_by(&:clicks)
   end 
 
   def highest_converted_clicks 
-    @results.select { |a| a[:id_campaing].strip != "--" }.max_by(&:converted_clicks)
+    select_data.max_by(&:converted_clicks)
   end 
 
   def lowest_cost 
-    @results.select { |a| a[:id_campaing].strip != "--" }.min_by(&:cost)
+    select_data.min_by(&:cost)
   end
 
   def highest_converted_clicks_div_clicks 
-    @results.select { |a| a[:id_campaing].strip != "--" }.max_by(&:converted_clicks_div_clicks)
+    select_data.max_by(&:converted_clicks_div_clicks)
   end  
 
   def lowest_cost_div_converted_click 
-    @results.select { |a| a[:id_campaing].strip != "--" }.min_by(&:cost_div_converted_click)
+    select_data.min_by(&:cost_div_converted_click)
+  end
+
+  private
+
+  def select_data
+    @results.select { |a| a[:id_campaing].strip != "--" }
   end
 
 end
