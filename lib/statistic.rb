7e7250ccb926335e15c472b47f26cf71ad5c1df3
Mary Gomez Villarreal <marygomez@192-168-1-11.tpgi.com.au>
2015-08-25 20:02:28 +1000

    Implement YAML for spec/statistic_spec.rb

--- lib/statistic.rb
+++ lib/statistic.rb
@@ -7,32 +7,32 @@ attr_reader :results
   end
 
   def highest_impressions 
-    select_data.max_by(&:impressions)
+    filter_data.max_by(&:impressions)
   end
 
   def highest_clicks  
-    select_data.max_by(&:clicks)
+    filter_data.max_by(&:clicks)
   end 
 
   def highest_converted_clicks 
-    select_data.max_by(&:converted_clicks)
+    filter_data.max_by(&:converted_clicks)
   end 
 
   def lowest_cost 
-    select_data.min_by(&:cost)
+    filter_data.min_by(&:cost)
   end
 
   def highest_converted_clicks_div_clicks 
-    select_data.max_by(&:converted_clicks_div_clicks)
+    filter_data.max_by(&:converted_clicks_div_clicks)
   end  
 
   def lowest_cost_div_converted_click 
-    select_data.min_by(&:cost_div_converted_click)
+    filter_data.min_by(&:cost_div_converted_click)
   end
 
   private
 
-  def select_data
+  def filter_data
     @results.select { |a| a[:id_campaing].strip != "--" }
   end
 
--- spec/statistic_spec.rb
+++ spec/statistic_spec.rb
@@ -1,10 +1,11 @@
 require 'statistic'
 require 'parse_csv'
-
+require 'yaml'
 
 describe Statistic do
 
   before do
+    @yaml = YAML.load_file("./fixtures/statistic.yml")
     parse = ParseCSV.new(:file_in => "./fixtures/test_data.csv")
     data = parse.parse_data
     @statistics = Statistic.new(:results => data)
@@ -12,32 +13,32 @@ describe Statistic do
 
   it "Highest Impressions" do
     highest_value = @statistics.highest_impressions
-    expect(highest_value[:impressions]).to eql(233)
+    expect(highest_value).to eql(@yaml["highest_impressions"])
   end
 
   it "Highest Clicks" do
     highest_value = @statistics.highest_clicks
-    expect(highest_value[:clicks]).to eql(7)
+    expect(highest_value).to eql(@yaml["highest_clicks"])
   end
 
   it "Highest Converted Clicks " do
     highest_value = @statistics.highest_converted_clicks
-    expect(highest_value[:converted_clicks]).to eql(1)
+    expect(highest_value).to eql(@yaml["highest_converted_clicks"])
   end
 
   it "Lowest Cost " do
     lowest_value = @statistics.lowest_cost
-    expect(lowest_value[:cost]).to eql(0)
+    expect(lowest_value).to eql(@yaml["lowest_cost"])
   end
 
   it "Highest Conv. Clicks/Clicks" do
     highest_value = @statistics.highest_converted_clicks_div_clicks
-    expect(highest_value[:converted_clicks_div_clicks]).to eql(25)
+    expect(highest_value).to eql(@yaml["highest_converted_clicks_div_clicks"])
   end
 
   it "Lowest Cost / Converted Click" do
     lowest_value = @statistics.lowest_cost_div_converted_click
-    expect(lowest_value[:cost_div_converted_click]).to eql(0)
+    expect(lowest_value).to eql(@yaml["lowest_cost_div_converted_click"])
   end
 
 end
